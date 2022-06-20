27bfc8d5e301a87e0f9cb9ba1a9dd124a2985d7c
abdulbaasitt <abdulbaasitsanusi@gmail.com>
2022-06-20 22:48:52 +0100

    paper

--- src/road_markings.cpp
+++ src/road_markings.cpp
@@ -16,21 +16,37 @@
 #include <pcl_ros/point_cloud.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
+#include <algorithm>
+#include <typeinfo>
+
+
+
 std_msgs::Header _velodyne_header;
+
+// int arg_max(std::vector const& vec) {
+//   return (int)(std::distance(vec.begin(), max_element(vec.begin(), vec.end())));
+// }
+
 
 unsigned int OTSU(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 {
 	unsigned int thrIntensity = 1;
 
 	/* 1 Intensity histogram */
-	unsigned int histogramIntensity[65536] = {0};
+	unsigned int histogramIntensity[256] = {0};
 	unsigned int maxIntensity = 0, minIntensity = 666666; // Maximum and minimum intensity values
+
+	int M = cloud->size();
 
 	for (pcl::PointCloud<pcl::PointXYZI>::iterator it = cloud->begin(); it != cloud->end(); it++)
 	{
 		// correct intensity to find corrected threshold
 
+	
+
 		unsigned int vIntensity = it->intensity;
+
+
 		if (vIntensity > maxIntensity)
 		{
 			maxIntensity = vIntensity;
@@ -42,44 +58,123 @@ unsigned int OTSU(const pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 		++histogramIntensity[vIntensity];
 	}
 
-	/* 2 total mass moment + = strength * points */
-	double sumIntensity = 0.0;
-	for (int k = minIntensity; k <= maxIntensity; k++)
+
+	double cumSum = 0.0;
+	double cumSumArray[maxIntensity] = {0};
+
+	for (int k = 0; k <= maxIntensity; k++)
 	{
-		sumIntensity += (double)k * (double)histogramIntensity[k];
+		cumSum += (double)histogramIntensity[k];
+		cumSumArray[k] = double(cumSum);
 	}
 
-	/* 3 traversal calculation */
-	double otsu = -1.0;
-	int w0 = 0;			  // The number of points less than or equal to the current threshold (number of previous scenic spots)
-	double sumFore = 0.0; // Foreground quality moment
-
-	unsigned int pcCount = cloud->size();
-	for (int k = minIntensity; k <= maxIntensity; k++)
+	double cumMean = 0.0;
+	double cumMeanArray[maxIntensity] = {0};
+	for (int k = 0; k <= maxIntensity; k++)
 	{
-		w0 += histogramIntensity[k];
+		cumMean += (double)k * (double)histogramIntensity[k];
+		cumMeanArray[k] = double(cumMean);
+	}
 
-		int w1 = pcCount - w0; //(the number of post-sites)
-		if (w0 == 0)
-			continue;
-		if (w1 == 0)
-			break;
+	double globalMean = 0.0;
+	double globalMeanArray[256]= {0};
 
-		sumFore += (double)k * histogramIntensity[k];
+	for (int k = 0; k <= 255; k++)
+	{
+		globalMean += (double)k * (double)histogramIntensity[k];
+		globalMeanArray[k]= double(globalMean);
+	}
 
-		double u0 = sumFore / w0;									// The average gray level of the foreground
-		double u1 = (sumIntensity - sumFore) / w1;					// The average gray level of the background
-		double g = (double)w0 * (double)w1 * (u0 - u1) * (u0 - u1); // variance between classes
+	double globalVar = 0.0;
+	double globalVarArray [256] = {0};
 
-		if (g > otsu)
-		{
-			otsu = g;
-			thrIntensity = k;
+	for (int k = 0; k <= 255; k++)
+	{
+		globalVar += (((double)k - globalMean) * ((double)k - globalMean) * (double)histogramIntensity[k]);
+		globalVarArray[k]= double(globalVar);
+	}
+
+	double localVariance = 0.0;
+	double localVarianceArray[maxIntensity] = {0};
+
+	for (int k = 0; k <= maxIntensity; k++)
+	{
+		int divisor = (  histogramIntensity[k]  * ( 1 - histogramIntensity[k] )  );
+
+		if (  divisor == 0 ){
+
+			localVariance = (  ( (globalMean * histogramIntensity[k]) - cumMeanArray[k] )  / 0.0000001 );
+
 		}
+		else {
+			
+			localVariance = (  ( (globalMean * histogramIntensity[k]) - cumMeanArray[k] )  /  (  histogramIntensity[k]  * ( 1 - histogramIntensity[k] )  ) );
+
+		}
+
+		// localVariance = (  ( (globalMean * histogramIntensity[k]) - cumMeanArray[k] )  /  (  histogramIntensity[k]  * ( 1 - histogramIntensity[k] )  ) );
+
+		localVarianceArray[k] = localVariance;
+
+	}
+	
+
+	 // // to find the argmax of the array
+
+	// double min = *std::max_element(localVarianceArray,localVarianceArray+maxIntensity);
+
+	// std::cout << "LOCAL_VARIANCE " << min<< '\n';
+
+	// std::cout << "Index of max element: "<< std::distance(localVarianceArray, std::max_element(localVarianceArray,localVarianceArray+maxIntensity))<< std::endl;
+
+	thrIntensity =std::distance(localVarianceArray, std::max_element(localVarianceArray,localVarianceArray+maxIntensity));
+	
+	return thrIntensity;
+
 	}
 
-	return thrIntensity;
-}
+
+
+// 	/* 2 total mass moment + = strength * points */
+// 	double sumIntensity = 0.0;
+// 	for (int k = minIntensity; k <= maxIntensity; k++)
+// 	{
+// 		sumIntensity += (double)k * (double)histogramIntensity[k];
+// 	}
+
+// 	/* 3 traversal calculation */
+// 	double otsu = -1.0;
+// 	int w0 = 0;			  // The number of points less than or equal to the current threshold (number of previous scenic spots)
+// 	double sumFore = 0.0; // Foreground quality moment
+
+// 	unsigned int pcCount = cloud->size();
+// 	for (int k = minIntensity; k <= maxIntensity; k++)
+// 	{
+// 		w0 += histogramIntensity[k];
+
+// 		int w1 = pcCount - w0; //(the number of post-sites)
+// 		if (w0 == 0)
+// 			continue;
+// 		if (w1 == 0)
+// 			break;
+
+// 		sumFore += (double)k * histogramIntensity[k];
+
+// 		double u0 = sumFore / w0;									// The average gray level of the foreground
+// 		double u1 = (sumIntensity - sumFore) / w1;					// The average gray level of the background
+// 		double g = (double)w0 * (double)w1 * (u0 - u1) * (u0 - u1); // variance between classes
+
+// 		if (g > otsu)
+// 		{
+// 			otsu = g;
+// 			thrIntensity = k;
+// 		}
+// 	}
+
+// 	return thrIntensity;
+// }
+
+
 /////////////////////
 void Correct_Intensity(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud_ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr out_cloud_ptr)
 {
@@ -116,6 +211,7 @@ void Filter(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud_ptr, pcl::PointC
 	{
 
 		if ((it->intensity > THRESHOLD) && (it->x > 0))
+		// if ((it->intensity < THRESHOLD) && (it->x > 0))
 		{
 			out_cloud_ptr->points.push_back(*it);
 		}
@@ -140,7 +236,7 @@ public:
 		pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_corrected(new pcl::PointCloud<pcl::PointXYZI>);
 
 		pcl::fromROSMsg(*cloud_msg, *cloud);
-		Correct_Intensity(cloud, cloud_corrected);
+		// Correct_Intensity(cloud, cloud_corrected);
 
 		unsigned int THRESHOLD;
 		THRESHOLD = OTSU(cloud);
